866a7925f90c830db9065927db989f03740a6b8a
Farhad TK <57707809+FarhadTK-77@users.noreply.github.com>
2022-08-13 15:28:05 +0430

    Uploading script

---
+++ scripts/app.js
@@ -0,0 +1,13 @@
+var menu = document.querySelector(".menu")
+var menuBtn = document.querySelector(".header__icon")
+var menuBtnIcon = document.querySelector(".header__icon i")
+
+menuBtn.addEventListener("click",function (){
+  if ( menuBtnIcon.classList.contains("fa-bars")){
+    menu.style.left = "0"
+    menuBtnIcon.classList = "fa fa-times"
+  } else {
+    menu.style.left = "-256px"
+    menuBtnIcon.classList = "fa fa-bars"
+  }
+})
